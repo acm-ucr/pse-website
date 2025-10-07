23e4731b3940dfc2da94ad2967a74d9b29a81b6c
Brandon Tran <brandontran1220@gmail.com>
2025-10-06 19:15:56 -0700

    Add hello world

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Hello from "@/components/hello";
+
 const Home = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      Hello World
+      <Hello />
     </div>
   );
 };

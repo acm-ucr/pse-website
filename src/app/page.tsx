1910e9e642a05f5c3c5cb71569ad55db306080ca
ZaydenGit <zaydenmiddleton12@gmail.com>
2025-10-07 13:34:16 -0700

    Add Example Component * Modified pages.tsx and added Example.tsx * this shit sucks i used absolute positioning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import ExampleComponent from "@/components/Example";
+
 const Home = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      Hello World
+      <ExampleComponent />
     </div>
   );
 };

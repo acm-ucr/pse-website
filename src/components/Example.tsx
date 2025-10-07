004b4ccf480f25dc5ad4e50f6ebe5a48f940aada
ZaydenGit <zaydenmiddleton12@gmail.com>
2025-10-07 13:45:40 -0700

    Changed absolute positioning to relative (so it doesn't suck anymore)

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,12 +1,12 @@
 const ExampleComponent = () => {
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center p-8">
-      <h1 className="pd-4 text-4xl text-red-600">EXAMPLE COMPONENT</h1>
-      <div className="mt-6 flex h-58 w-128 border-2">
+    <div className="flex h-screen w-screen flex-col justify-center p-8">
+      <h1 className="pd-4 mx-auto text-4xl text-red-600">EXAMPLE COMPONENT</h1>
+      <div className="mx-auto mt-6 flex h-58 w-full max-w-lg border-2">
         <p className="flex-1 bg-orange-300" />
         <p className="flex-1 bg-blue-300" />
       </div>
-      <div className="mt-4 ml-172 w-full text-left">
+      <div className="mx-auto mt-4 flex w-full max-w-lg text-left">
         <p className="flex text-lg font-bold italic">Example Component</p>
       </div>
     </div>

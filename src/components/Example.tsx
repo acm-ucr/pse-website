db47338d0daacc887ae8fbe9f2e581f3f63731e0
ZaydenGit <zaydenmiddleton12@gmail.com>
2025-10-07 22:17:25 -0700

    I made it look very accurate

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,13 +1,15 @@
 const ExampleComponent = () => {
   return (
     <div className="flex h-screen w-screen flex-col justify-center p-8">
-      <h1 className="pd-4 mx-auto text-4xl text-red-600">EXAMPLE COMPONENT</h1>
-      <div className="mx-auto mt-6 flex h-58 w-full max-w-lg border-2">
+      <h1 className="pd-4 mx-auto text-5xl text-red-600">EXAMPLE COMPONENT</h1>
+      <div className="mx-auto mt-8 flex h-75 w-full max-w-179 border-2">
         <p className="flex-1 bg-orange-300" />
         <p className="flex-1 bg-blue-300" />
       </div>
-      <div className="mx-auto mt-4 flex w-full max-w-lg text-left">
-        <p className="flex text-lg font-bold italic">Example Component</p>
+      <div className="mx-auto mt-7 flex w-full max-w-178 text-left">
+        <p className="flex text-lg font-extrabold text-black italic">
+          Example Component
+        </p>
       </div>
     </div>
   );

1cb18c001a4d391c9def9d62e60194824c214bd1
djechi <djechipare@gmail.com>
2025-10-07 20:56:41 -0700

    Forgot to save after formatting with prettier i think

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,18 +1,20 @@
 const Example = () => {
-    return(
-        <div className="h-screen w-screen flex flex-col items-center justify-center">
-            <h1 className= "text-red-600 text-7xl mb-12" >EXAMPLE COMPONENT</h1>
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center">
+      <h1 className="mb-12 text-7xl text-red-600">EXAMPLE COMPONENT</h1>
 
-            <div className="space-y-12">
-                <div className="border-4 border-black flex">
-                    <div className="w-[550px] h-[450px] bg-orange-300"/>
-                    <div className="w-[550px] h-[450px] bg-blue-300"/>
-                </div>
-                
-                <h4 className="text-black text-3xl font-extrabold italic">Example Component</h4>
-            </div>
+      <div className="space-y-12">
+        <div className="flex border-4 border-black">
+          <div className="h-[450px] w-[550px] bg-orange-300" />
+          <div className="h-[450px] w-[550px] bg-blue-300" />
         </div>
-    )
-}
+
+        <h4 className="text-3xl font-extrabold text-black italic">
+          Example Component
+        </h4>
+      </div>
+    </div>
+  );
+};
 
 export default Example;

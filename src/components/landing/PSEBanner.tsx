48c881c98f0a0dc2fa15601df75ea1c779f02b95
ZaydenGit <zaydenmiddleton12@gmail.com>
2025-10-15 19:06:52 -0700

    Changed scaling slightly and updated font of subtitle to Maitree (according to Figma)

--- src/components/landing/PSEBanner.tsx
+++ src/components/landing/PSEBanner.tsx
@@ -10,13 +10,13 @@ const PSEBanner = () => {
       <Image
         src={PSEBg}
         alt="PSE Background"
-        className="absolute inset-0 top-[40px] -z-10 flex w-full scale-150 object-cover transition-transform duration-300 ease-linear sm:-top-[20px] sm:scale-120"
+        className="absolute inset-0 top-[15px] -z-10 flex w-full scale-140 object-cover transition-transform duration-300 ease-linear sm:-top-[20px] sm:scale-120"
       />
       <div className="bg-pse-purple-300/75 absolute inset-0 w-full"></div>
       <p className="font-pse-crimson-text scale-50 text-center text-[125px] whitespace-nowrap text-white transition-transform duration-300 ease-linear sm:scale-60 md:scale-75">
         Pi Sigma Epsilon
       </p>
-      <p className="font-pse-gfs-didot z-10 -mt-18 scale-50 text-center text-[40px] whitespace-nowrap text-white transition-transform duration-300 ease-linear sm:-mt-16 sm:scale-60 md:-mt-10 md:scale-75">
+      <p className="font-pse-maitree z-10 -mt-18 scale-50 text-center text-[40px] whitespace-nowrap text-white transition-transform duration-300 ease-linear sm:-mt-16 sm:scale-60 md:-mt-10 md:scale-75">
         University of California, Riverside Chapter
       </p>
     </div>

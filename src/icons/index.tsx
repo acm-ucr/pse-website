b91ac2aff9a952bbacf2858f187c7188969f96f7
LivayaaSharma <livayaasharma777@gmail.com>
2025-10-18 19:51:59 -0700

    Used react-icons for linktree, fixed links for the socials, removed discord

--- src/icons/index.tsx
+++
@@ -1,7 +0,0 @@
-// src/icons/index.tsx
-
-// Font Awesome imports
-import { FaInstagram, FaLinkedin, FaDiscord } from "react-icons/fa";
-
-// Export them for easy imports
-export { FaInstagram, FaLinkedin, FaDiscord };

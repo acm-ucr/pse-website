d5261ac79b20409244ebdd7def97bb4327720c39
gurjot singh <har03gur@gmail.com>
2025-10-19 18:56:20 -0700

    Added href, text, and color props to the button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,11 @@ import JoinUsButton from "@/components/landing/JoinUsButton";
 const Home = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <JoinUsButton 
-      link="https://docs.google.com/forms/d/e/1FAIpQLSco0HkuBMDgfyNt2JmmyuYf9OYI5aGxlGIkl4B9omNPD74zWw/viewform" 
-      text="JOIN US!" 
-      color = "bg-pse-yellow-100"/>
+      <JoinUsButton
+        link="https://docs.google.com/forms/d/e/1FAIpQLSco0HkuBMDgfyNt2JmmyuYf9OYI5aGxlGIkl4B9omNPD74zWw/viewform"
+        text="JOIN US!"
+        color="bg-pse-yellow-100"
+      />
     </div>
   );
 };
--- src/components/landing/JoinUsButton.tsx
+++ src/components/landing/JoinUsButton.tsx
@@ -6,12 +6,16 @@ interface JoinUsButtonProps {
   color: string;
 }
 
-const JoinUsButton = ({link, text, color}: JoinUsButtonProps) => {
+const JoinUsButton = ({ link, text, color }: JoinUsButtonProps) => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       <Link
         href={link}
-        className={"font-pse-maitree rounded-xl " + color + " px-18 py-5 text-3xl tracking-wider text-pse-black-100"}
+        className={
+          "font-pse-maitree rounded-xl " +
+          color +
+          " text-pse-black-100 px-18 py-5 text-3xl tracking-wider"
+        }
       >
         {text}
       </Link>

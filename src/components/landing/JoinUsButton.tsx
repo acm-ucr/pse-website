9c9263d2dea3e9cc60b31e10023e1e3b579e5ee5
Gurjot Singh <47932844+GurjotSDev@users.noreply.github.com>
2025-10-19 17:32:44 -0700

    Update src/components/landing/JoinUsButton.tsx
    Co-authored-by: Brandon Tran <125536574+brandontran1220@users.noreply.github.com>

--- src/components/landing/JoinUsButton.tsx
+++ src/components/landing/JoinUsButton.tsx
@@ -6,7 +6,7 @@ const JoinUsButton = () => {
         className="font-pse-gfs-didot rounded-xl bg-[#FFC653] px-18 py-5 text-3xl tracking-wider text-[#2A2A2A]"
       >
         JOIN US!
-      </a>
+      </Link>
     </div>
   );
 };

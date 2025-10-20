9017c37a634e7b461528c4f3499dc88a2b04f553
Gurjot Singh <47932844+GurjotSDev@users.noreply.github.com>
2025-10-19 17:32:56 -0700

    Update src/components/landing/JoinUsButton.tsx
    Co-authored-by: Brandon Tran <125536574+brandontran1220@users.noreply.github.com>

--- src/components/landing/JoinUsButton.tsx
+++ src/components/landing/JoinUsButton.tsx
@@ -1,7 +1,7 @@
 const JoinUsButton = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <a
+      <Link
         href="@/join-us"
         className="font-pse-gfs-didot rounded-xl bg-[#FFC653] px-18 py-5 text-3xl tracking-wider text-[#2A2A2A]"
       >

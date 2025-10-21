15d5fc3aff069f3c2112410d917459b7f417b13c
minjlys <lymifrog@gmail.com>
2025-10-20 19:20:35 -0700

    principles section made larger

--- src/components/landing/Principles.tsx
+++ src/components/landing/Principles.tsx
@@ -16,15 +16,15 @@ const principles = [
 const Principles = () => {
   return (
     <div className="flex min-h-screen flex-col items-start justify-center px-20 py-10">
-      <div className="font-pse-crimson-text text-pse-purple-400 mb-8 text-5xl">
+      <div className="font-pse-crimson-text text-pse-purple-400 mb-12 text-7xl">
         Our Principles
       </div>
 
-      <div className="grid grid-cols-3 gap-8">
+      <div className="grid w-full grid-cols-3 gap-8">
         {principles.map((name) => (
           <div
             key={name}
-            className="bg-pse-purple-100 font-pse-maitree flex h-16 w-60 items-center justify-center rounded-xl text-xl font-semibold shadow-lg"
+            className="bg-pse-purple-100 font-pse-maitree flex h-[6.5rem] w-[25rem] items-center justify-center rounded-2xl text-4xl font-medium shadow-lg"
           >
             {name}
           </div>

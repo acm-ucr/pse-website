641a0dc6fb89d0fc6e80d0f8d04abe929dc81ae6
minjlys <lymifrog@gmail.com>
2025-10-20 22:28:09 -0700

    principles section enlarged

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import Principles from "@/components/landing/Principles";
+
 const Home = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
--- src/components/landing/Principles.tsx
+++ src/components/landing/Principles.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 const principles = [
   "Competition",
   "Sincerity",

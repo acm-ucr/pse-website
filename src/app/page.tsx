54a9aecaaadc19431ed98c02e8d4a74511defce3
Tony Liu <tonyliu05@hotmail.com>
2025-10-21 13:12:58 -0700

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import Navbar from "@/components/ui/navbar";
-import JoinUsButton from "@/components/landing/JoinUsButton";
+import Principles from "@/components/landing/Principles";
 const Home = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       <Navbar />
-      <JoinUsButton
+      {/*<JoinUsButton
         link="https://docs.google.com/forms/d/e/1FAIpQLSco0HkuBMDgfyNt2JmmyuYf9OYI5aGxlGIkl4B9omNPD74zWw/viewform"
         text="JOIN US!"
         color="bg-pse-yellow-100"

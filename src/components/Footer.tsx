cb3188b397d20c52ebeb66acb890f8710d74e8b1
LivayaaSharma <livayaasharma777@gmail.com>
2025-10-21 15:08:58 -0700

    Addressed styling changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,20 +6,22 @@ import { SiLinktree } from "react-icons/si";
 
 export default function Footer() {
   return (
-    <div className="bg-pse-black-100 w-full p-8 text-white">
-      <div className="grid grid-cols-2 items-start gap-2">
-        <div className="flex flex-col">
-          <div className="flex items-center gap-2">
+    <div className="bg-pse-black-100 w-full px-14 py-8 text-white">
+      <div className="grid grid-cols-2 items-center gap-2">
+        <div className="flex flex-col justify-center">
+          <div className="flex items-center gap-3">
             <Image
               src={logo}
               alt="PSE Logo"
-              width={32}
-              height={32}
+              width={45}
+              height={45}
               className="object-contain"
             />
-            <p className="text-pse-yellow-100 text-2xl font-semibold">ΠΣΕ</p>
+            <p className="text-pse-yellow-100 font-pse-gfs-didot text-5xl font-medium">
+              ΠΣΕ
+            </p>
           </div>
-          <p className="text-md">
+          <p className="font-pse-gfs-didot text-xl">
             © 2025 Pi Sigma Epsilon — Theta Gamma Chapter
           </p>
         </div>
@@ -31,7 +33,7 @@ export default function Footer() {
               target="_blank"
               className="transition hover:opacity-80"
             >
-              <FaInstagram size={24} />
+              <FaInstagram size={35} />
             </Link>
 
             <Link
@@ -39,7 +41,7 @@ export default function Footer() {
               target="_blank"
               className="transition hover:opacity-80"
             >
-              <FaLinkedin size={24} />
+              <FaLinkedin size={35} />
             </Link>
 
             <Link
@@ -47,13 +49,13 @@ export default function Footer() {
               target="_blank"
               className="transition hover:opacity-80"
             >
-              <SiLinktree size={24} />
+              <SiLinktree size={35} />
             </Link>
           </div>
 
           <Link
             href="mailto:pres.pseatucr@gmail.com"
-            className="text-md hover:underline"
+            className="font-pse-gfs-didot text-xl hover:underline"
           >
             pres.pseatucr@gmail.com
           </Link>

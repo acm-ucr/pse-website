038d7782bad9d58a424ca48ef132c8d3c1706dc3
Tony Liu <tonyliu05@hotmail.com>
2025-10-24 00:07:45 -0700

    fixes

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,19 +1,18 @@
 "use client";
 const NotFound = () => {
   return (
-    <div className="text-pse-purple-400 flex min-h-screen flex-col items-center justify-center bg-white text-center font-light">
+    <div className="text-pse-purple-400 font-pse-crimson-text flex min-h-screen flex-col items-center justify-center bg-white text-center font-light">
       <div className="text-[15rem] leading-none font-light">404</div>
-      <p className="text-pse-purple-200 mt-4 text-5xl font-medium">
+      <p className="text-pse-purple-300 font-pse-gfs-display mt-4 text-5xl font-medium">
         Page Not Found
       </p>
       <a
         href="/"
-        className="bg-pse-purple-400 hover:bg-pse-purple-300 mt-8 rounded-xl px-8 py-3 text-xl text-white transition"
+        className="bg-pse-purple-400 hover:bg-pse-purple-300 mt-8 rounded-xl px-8 py-3 text-3xl text-white transition"
       >
         Home
       </a>
     </div>
   );
 };
-
 export default NotFound;

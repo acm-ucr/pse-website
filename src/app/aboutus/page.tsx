105bc5a7eab8997af8be86af50a92da1f75df512
djechi <djechipare@gmail.com>
2025-10-29 15:52:26 -0700

    Update page format

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -2,9 +2,9 @@ import TitleComp from "@/components/TitleComp";
 
 const AboutUs = () => {
   return (
-    <div className="">
+    <>
       <TitleComp title="About Us" />;
-    </div>
+    </>
   );
 };
 

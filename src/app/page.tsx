057be2895411744698c9cc7261547a444f352045
djechi <djechipare@gmail.com>
2025-10-29 13:16:21 -0700

    Restore page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import Principles from "@/components/landing/Principles";
+
+const Home = () => {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      {/*<JoinUsButton
+        link="https://docs.google.com/forms/d/e/1FAIpQLSco0HkuBMDgfyNt2JmmyuYf9OYI5aGxlGIkl4B9omNPD74zWw/viewform"
+        text="JOIN US!"
+        color="bg-pse-yellow-100"
+      />*/}
+      <Principles />
+    </div>
+  );
+};
+
+export default Home;

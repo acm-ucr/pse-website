3f149cece3ffa317b68f4c0c86e903fd5461d82d
Tony Liu <tonyliu05@hotmail.com>
2025-11-02 01:20:46 -0700

    fix 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,18 +1,26 @@
 "use client";
+import Button from "@/components/Button";
+
 const NotFound = () => {
   return (
-    <div className="text-pse-purple-400 font-pse-crimson-text flex min-h-screen flex-col items-center justify-center bg-white text-center font-light">
-      <div className="text-[15rem] leading-none font-light">404</div>
-      <p className="text-pse-purple-300 font-pse-gfs-display mt-4 text-5xl font-medium">
-        Page Not Found
-      </p>
-      <a
-        href="/"
-        className="bg-pse-purple-400 hover:bg-pse-purple-300 mt-8 rounded-xl px-8 py-3 text-3xl text-white transition"
-      >
-        Home
-      </a>
+    <div className="font-pse-crimson-text flex min-h-screen flex-col items-center justify-center space-y-4 bg-white text-center font-light">
+      <div className="flex flex-col items-center justify-center space-y-2">
+        <p className="text-pse-purple-500 text-[15rem] leading-none font-light">
+          404
+        </p>
+        <p className="text-pse-purple-200 text-5xl font-medium">
+          Page Not Found
+        </p>
+      </div>
+      <div className="relative h-auto w-auto overflow-hidden">
+        <Button
+          link="/"
+          text="Home"
+          color="bg-pse-purple-500 hover:bg-pse-purple-600 text-white"
+        />
+      </div>
     </div>
   );
 };
+
 export default NotFound;

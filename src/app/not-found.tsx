9e094b755c9d031c714922d8936c9bbf4515b36a
Sweden <84001329+swedenagu@users.noreply.github.com>
2025-11-10 09:43:57 -0800

    Apply suggestions from Brandon's code review
    Co-authored-by: Brandon Tran <125536574+brandontran1220@users.noreply.github.com>

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,21 +4,19 @@ import Button from "@/components/Button";
 const NotFound = () => {
   return (
     <div className="font-pse-crimson-text flex min-h-screen flex-col items-center justify-center bg-white text-center font-light">
-      <div className="flex flex-col items-center justify-center space-y-3">
+      <div className="flex flex-col items-center justify-center">
         <p className="text-pse-purple-500 text-[15rem] leading-none font-light">
           404
         </p>
-        <p className="text-pse-purple-200 mt-4 text-5xl font-medium">
+        <p className="text-pse-purple-250 mt-4 mb-12 text-5xl font-medium">
           Page Not Found
         </p>
       </div>
-      <div className="mt-20 !h-auto !w-auto overflow-visible [&>div]:!h-auto [&>div]:!w-auto [&>div]:!flex-none">
         <Button
           link="/"
           text="Home"
           color="bg-pse-purple-500 hover:bg-pse-purple-600 text-white"
         />
-      </div>
     </div>
   );
 };

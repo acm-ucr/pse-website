b7e166ef8335195a6631e1efc77ab685878f490a
Sweden <sweden.agunenye1@gmail.com>
2025-11-10 13:01:35 -0800

    restaged aboutus files

--- src/components/aboutUs/Description.tsx
+++ src/components/aboutUs/Description.tsx
@@ -1,6 +1,6 @@
 const Description = () => {
   return (
-    <div className="flex w-full items-center justify-center bg-white">
+    <div className="flex flex-col w-full items-center justify-center bg-white">
       <p className="font-pse-maitree my-[68px] text-center text-3xl leading-10.5 font-normal">
         PSE at UCR is dedicated to empowering students with <br />
         real-world business experience, fostering leadership, and <br />

87d40c96bab049150ac312fda2f95112399139df
Sweden <sweden.agunenye1@gmail.com>
2025-11-10 14:18:45 -0800

    button isn't supposed to be on landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,10 @@
 import PSEBanner from "@/components/landing/PSEBanner";
 import Principles from "@/components/landing/Principles";
 import OurMission from "@/components/landing/ourmission";
-import Button from "@/components/landing/Button";
 
 const Home = () => {
   return (
     <div className="flex w-full flex-col items-center justify-start gap-16 pb-10">
-      <Button
-        link="https://docs.google.com/forms/d/e/1FAIpQLSco0HkuBMDgfyNt2JmmyuYf9OYI5aGxlGIkl4B9omNPD74zWw/viewform"
-        text="JOIN US!"
-        color="bg-pse-yellow-100"
-      />
       <PSEBanner />
       <OurMission />
       <Principles />

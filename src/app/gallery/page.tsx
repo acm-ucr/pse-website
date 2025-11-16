d6befb5b48f471b825a244239b462f6e6738cabf
brandontran1220 <brandontran1220@gmail.com>
2025-11-16 14:42:11 -0800

    Add gallery page

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import TitleComp from "@/components/TitleComp";
+import Gallery from "@/components/gallery/Gallery";
+
+const Home = () => {
+  return (
+    <>
+      <TitleComp title="Gallery" />
+      <Gallery />
+    </>
+  );
+};
+
+export default Home;

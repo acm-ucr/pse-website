faf63e0f1bde6892f891221e8ce06b2467d3f61b
brandontran1220 <brandontran1220@gmail.com>
2025-11-16 13:45:12 -0800

    Fix some bugs

--- src/app/join/page.tsx
+++ src/app/join/page.tsx
@@ -1,8 +1,12 @@
 import Testimonials from "@/components/join/testimonials";
+import InterestSection from "@/components/landing/InterestSection";
+import TitleComp from "@/components/TitleComp";
 
 const JoinUs = () => {
   return (
     <>
+      <TitleComp title="Join US" />
+      <InterestSection />
       <Testimonials />
     </>
   );

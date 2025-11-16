c39342be4faa88da62a59dbcbbd63a574f261ab9
brandontran1220 <brandontran1220@gmail.com>
2025-11-16 14:08:37 -0800

    add images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,11 @@ import OurMission from "@/components/landing/ourmission";
 
 const Home = () => {
   return (
-    <div className="flex w-full flex-col items-center justify-start gap-16 pb-10">
+    <>
       <PSEBanner />
       <OurMission />
       <Principles />
-    </div>
+    </>
   );
 };
 

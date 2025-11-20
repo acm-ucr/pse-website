b9df6c30afe508d7d4fec92625f96dd1db780442
Howard Wheeler <167825355+Howardwheeler@users.noreply.github.com>
2025-11-20 07:31:15 +0000

    calendar commit 1

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,29 +1,29 @@
 "use client";
 import TitleComp from "@/components/TitleComp";
 import EventPageCard from "@/components/calendar/EventPageCard";
-import Calendar from "@/components/calendar/calendarcall"
+import Calendar from "@/components/calendar/calendarcall";
 
 const EventPage = () => {
   return (
     <>
-    <TitleComp title="Calendar" />
-    <div className="flex w-full flex-col items-center">
-      <EventPageCard
-        title="Event Name"
-        description="Lorem ipsum dolor sit amet consectetur adipiscing elit. Consectetur adipiscing elit quisque faucibus ex sapien vitae. Ex sapien vitae pellentesque sem placerat in id"
-        date="JUL 21"
-        time="7:00 PM"
-        location="HUB 265"
-      />
-      <EventPageCard
-        title="Event Name"
-        description="Lorem ipsum dolor sit amet consectetur adipiscing elit. Consectetur adipiscing elit quisque faucibus ex sapien vitae. Ex sapien vitae pellentesque sem placerat in id"
-        date="JUL 21"
-        time="7:00 PM"
-        location="HUB 265"
-      />
-    </div>
-    <Calendar />
+      <TitleComp title="Calendar" />
+      <div className="flex w-full flex-col items-center">
+        <EventPageCard
+          title="Event Name"
+          description="Lorem ipsum dolor sit amet consectetur adipiscing elit. Consectetur adipiscing elit quisque faucibus ex sapien vitae. Ex sapien vitae pellentesque sem placerat in id"
+          date="JUL 21"
+          time="7:00 PM"
+          location="HUB 265"
+        />
+        <EventPageCard
+          title="Event Name"
+          description="Lorem ipsum dolor sit amet consectetur adipiscing elit. Consectetur adipiscing elit quisque faucibus ex sapien vitae. Ex sapien vitae pellentesque sem placerat in id"
+          date="JUL 21"
+          time="7:00 PM"
+          location="HUB 265"
+        />
+      </div>
+      <Calendar />
     </>
   );
 };

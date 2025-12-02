2aa9b50475decd45b39df9ebf62f3b4ac3026429
Brandon Tran <brandontran1220@gmail.com>
2025-12-02 00:05:21 -0800

    bruh

--- src/components/overviews/EventSections.tsx
+++ src/components/overviews/EventSections.tsx
@@ -3,7 +3,7 @@ import { EventSection } from "./EventSection";
 
 export function EventSections() {
   return (
-    <main className="space-y-16 md:px-4 md:py-10 py-6">
+    <main className="space-y-16 py-6 md:px-4 md:py-10">
       {events.map((event, index) => (
         <EventSection key={index} {...event} />
       ))}
